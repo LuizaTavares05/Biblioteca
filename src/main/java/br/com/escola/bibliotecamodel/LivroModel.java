92d3efdef2c78d70abccab45258b6d75a90fdb17
betarbadaro <betarbadaro@gmail.com>
2026-05-22 19:49:58 -0300

    Atualização de Regras do Trabalho

--- src/main/java/br/com/escola/bibliotecamodel/LivroModel.java
+++ src/main/java/br/com/escola/bibliotecamodel/LivroModel.java
@@ -1,100 +1,90 @@
 package br.com.escola.bibliotecamodel;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table
 public class LivroModel {
-	
-	@Id
-	@Generated (value = { "" })
-	private Long Id;
-	@Column
-	private String titulo;
-	@Column
-	private String isbn;
-	@Column
-	private String anoPublicacao;
-	@Column
-	private String genero;
-	@Column
-	private String autor;
-	
-	
-	
-	public LivroModel() {
-		super();
-	}
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) /*Banco gera ID sozinho */
+    private Long id;
 
-	public LivroModel(Long id, String titulo, String isbn, String anoPublicacao, String genero, String autor) {
-		super();
-		Id = id;
-		this.titulo = titulo;
-		this.isbn = isbn;
-		this.anoPublicacao = anoPublicacao;
-		this.genero = genero;
-		this.autor = autor;
-	}
+    @Column(name = "titulo", nullable = false) /*Regras Obrigatorias - Não permitir cadastro de livro com titulo vazio */
+    private String titulo;
 
+    @Column
+    private String isbn;
 
-	public Long getId() {
-		return Id;
-	}
+    @Column
+    private String anoPublicacao;
 
+    @Column
+    private String genero;
 
-	public String getTitulo() {
-		return titulo;
-	}
+    @Column(name = "autor", nullable = false)/*Regras Obrigatorias - Não permitir cadastro de livro sem autor*/
+    private String autor;
 
+    public LivroModel() {
+        super();
+    }
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    public LivroModel(Long id, String titulo, String isbn, String anoPublicacao, String genero, String autor) {
+        super();
+        this.id = id;
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.anoPublicacao = anoPublicacao;
+        this.genero = genero;
+        this.autor = autor;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
-	public String getIsbn() {
-		return isbn;
-	}
+    public String getTitulo() {
+        return titulo;
+    }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
+    public String getIsbn() {
+        return isbn;
+    }
 
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
-	public String getAnoPublicacao() {
-		return anoPublicacao;
-	}
+    public String getAnoPublicacao() {
+        return anoPublicacao;
+    }
 
+    public void setAnoPublicacao(String anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
 
-	public void setAnoPublicacao(String anoPublicacao) {
-		this.anoPublicacao = anoPublicacao;
-	}
+    public String getGenero() {
+        return genero;
+    }
 
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 
-	public String getGenero() {
-		return genero;
-	}
+    public String getAutor() {
+        return autor;
+    }
 
-
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
-
-
-	public String getAutor() {
-		return autor;
-	}
-
-
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
-	
-
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
 }
