6e2f1495355dee3976e6e429eada3c6340eacb1a
Gabriel Mendonça <gabriel.mendonca.d.s.m@gmail.com>
2026-05-23 18:13:46 -0300

    Pequenas mudanças no LivroController e mudei o 'autor' para 'Autor'

--- src/main/java/br/com/escola/biblioteca/Controller/LivroController.java
+++ src/main/java/br/com/escola/biblioteca/Controller/LivroController.java
@@ -1,9 +1,10 @@
-package br.com.escola.biblioteca.controller;
+package br.com.escola.biblioteca.Controller;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,11 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.escola.biblioteca.repository.LivroRepository;
+
 @RestController
 @RequestMapping("/livro")
 public class LivroController {
 	/*
 	private static List<Livro> lista_livro = new ArrayList<Livro>();
+	
+	@Autowired
+	public LivroRepository repositorio;
 	
 	@PostMapping("/inserir")
 	@ResponseStatus(HttpStatus.CREATED)
