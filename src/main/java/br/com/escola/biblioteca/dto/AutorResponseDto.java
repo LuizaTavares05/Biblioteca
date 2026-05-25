b6c9b5d8a8628db427d61bc42fe6e66b8e23b589
betarbadaro <betarbadaro@gmail.com>
2026-05-25 17:03:45 -0300

    Adicionando swagger nos controller e dtos

--- src/main/java/br/com/escola/biblioteca/dto/AutorResponseDto.java
+++ src/main/java/br/com/escola/biblioteca/dto/AutorResponseDto.java
@@ -1,12 +1,43 @@
 package br.com.escola.biblioteca.dto;
 
 import br.com.escola.biblioteca.model.AutorModel;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDate;
 
-public record AutorResponseDto(Long id, String nome, String nacionalidade, LocalDate dataNascimento) {
+public record AutorResponseDto(
 
-    public static AutorResponseDto fromEntity(AutorModel autor) {
+		 @Schema(
+		            description = "ID do autor",
+		            example = "1"
+		        )
+		        Long id,
+
+		        @Schema(
+		            description = "Nome do autor",
+		            example = "Machado de Assis"
+		        )
+		        String nome,
+
+		        @Schema(
+		            description = "Nacionalidade do autor",
+		            example = "Brasileira"
+		        )
+		        String nacionalidade,
+
+		        @Schema(
+		            description = "Data de nascimento do autor",
+		            example = "1839-06-21"
+		        )
+		        LocalDate dataNascimento
+
+		) {
+	
+	
+	
+	
+	
+	public static AutorResponseDto fromEntity(AutorModel autor) {
         return new AutorResponseDto(
                 autor.getId(),
                 autor.getNome(),
--- src/main/java/br/com/escola/biblioteca/dto/LivroResponseDTO.java
+++ src/main/java/br/com/escola/biblioteca/dto/LivroResponseDTO.java
@@ -1,11 +1,58 @@
 package br.com.escola.biblioteca.dto;
 
-import br.com.escola.biblioteca.model.LivroModel;;
+import br.com.escola.biblioteca.model.LivroModel;
 
-public record LivroResponseDTO( Long id, String titulo, String isbn, Integer anoPublicacao, String genero, 
-    Long autorId, String autorNome) {
+import io.swagger.v3.oas.annotations.media.Schema;
 
-    public static LivroResponseDTO fromEntity(LivroModel livro) {
+public record LivroResponseDTO( 
+   
+		   @Schema(
+		            description = "ID do livro",
+		            example = "1"
+		        )
+		        Long id,
+
+		        @Schema(
+		            description = "Título do livro",
+		            example = "Dom Casmurro"
+		        )
+		        String titulo,
+
+		        @Schema(
+		            description = "Código ISBN do livro",
+		            example = "978-85-359-0277-5"
+		        )
+		        String isbn,
+
+		        @Schema(
+		            description = "Ano de publicação do livro",
+		            example = "1899"
+		        )
+		        Integer anoPublicacao,
+
+		        @Schema(
+		            description = "Gênero literário do livro",
+		            example = "Romance"
+		        )
+		        String genero,
+
+		        @Schema(
+		            description = "ID do autor",
+		            example = "1"
+		        )
+		        Long autorId,
+
+		        @Schema(
+		            description = "Nome do autor",
+		            example = "Machado de Assis"
+		        )
+		        String autorNome
+		        
+		        ) {
+	
+
+	
+	public static LivroResponseDTO fromEntity(LivroModel livro) {
         return new LivroResponseDTO(
                 livro.getId(),
                 livro.getTitulo(),
