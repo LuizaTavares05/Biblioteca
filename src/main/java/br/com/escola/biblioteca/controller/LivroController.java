2881352ac2d78706a9edd3dba90d5c364d7f7efa
Luiza Tavares <137509063+luluzinha05@users.noreply.github.com>
2026-05-26 23:40:10 -0300

    ajustes

--- src/main/java/br/com/escola/biblioteca/controller/LivroController.java
+++ src/main/java/br/com/escola/biblioteca/controller/LivroController.java
@@ -19,10 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/livros")
 
-@Tag (
-		name = "livros",
-		description = "API responsavel pelo gerenciamento de livros"
-		)
+@Tag(name = "livros", description = "API responsavel pelo gerenciamento de livros")
 
 public class LivroController {
 
@@ -30,67 +27,54 @@ public class LivroController {
 	private LivroService livroService;
 
 	@PostMapping("/cadastrar")
-	
-	@Operation (
-			summary = "Cadastrar livro",
-			description = "Realiza o cadastro de um novo livro"
-			
-			)
-	   @ApiResponses(value = {
-		        @ApiResponse(responseCode = "201", description = "Livro cadastrado com sucesso"),
-		        @ApiResponse(responseCode = "400", description = "Erro ao cadastrar livro")
-		    })
-	
-	
-	
+
+	@Operation(summary = "Cadastrar livro", description = "Realiza o cadastro de um novo livro"
+
+	)
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Livro cadastrado com sucesso"),
+			@ApiResponse(responseCode = "400", description = "Erro ao cadastrar livro")
+	})
+
 	public ResponseEntity<LivroResponseDTO> inserir(@RequestBody @Valid LivroRequestDto dto) {
 		LivroResponseDTO response = livroService.criar(dto);
-		
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
 	@GetMapping("/listar")
-	
-	  @Operation(
-		        summary = "Listar livros",
-		        description = "Retorna todos os livros cadastrados"
-		    )
 
-		    @ApiResponses(value = {
-		        @ApiResponse(responseCode = "200", description = "Lista retornada com sucesso")
-		    })
-	
+	@Operation(summary = "Listar livros", description = "Retorna todos os livros cadastrados")
+
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Lista retornada com sucesso")
+	})
+
 	public ResponseEntity<List<LivroResponseDTO>> listar() {
 		return ResponseEntity.ok(livroService.listarTodos());
 	}
 
 	@GetMapping("/buscarPorId/{id}")
-	
-	@Operation(
-	        summary = "Buscar livro por ID",
-	        description = "Retorna um livro específico pelo ID"
-	    )
 
-	    @ApiResponses(value = {
-	        @ApiResponse(responseCode = "200", description = "Livro encontrado"),
-	        @ApiResponse(responseCode = "404", description = "Livro não encontrado")
-	    })
+	@Operation(summary = "Buscar livro por ID", description = "Retorna um livro específico pelo ID")
+
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Livro encontrado"),
+			@ApiResponse(responseCode = "404", description = "Livro não encontrado")
+	})
 	public ResponseEntity<LivroResponseDTO> buscar(@PathVariable Long id) {
 		return ResponseEntity.ok(livroService.buscarPorId(id));
 	}
 
 	@PutMapping("/alterarPorId/{id}")
-	
-	   @Operation(
-		        summary = "Alterar livro",
-		        description = "Atualiza os dados de um livro"
-		    )
 
-		    @ApiResponses(value = {
-		        @ApiResponse(responseCode = "200", description = "Livro atualizado com sucesso"),
-		        @ApiResponse(responseCode = "404", description = "Livro não encontrado")
-		    })
-	
+	@Operation(summary = "Alterar livro", description = "Atualiza os dados de um livro")
+
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Livro atualizado com sucesso"),
+			@ApiResponse(responseCode = "404", description = "Livro não encontrado")
+	})
+
 	public ResponseEntity<LivroResponseDTO> editar(@PathVariable Long id,
 			@RequestBody @Valid LivroRequestDto dto) {
 		return ResponseEntity.ok(livroService.atualizar(id, dto));
