e05695b352db5e70bb181080ee9aeb1b738d5820
Luiza Tavares <137509063+luluzinha05@users.noreply.github.com>
2026-05-27 21:22:13 -0300

    Ajustes finais

--- src/main/java/br/com/escola/biblioteca/repository/LivroRepository.java
+++ src/main/java/br/com/escola/biblioteca/repository/LivroRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.escola.biblioteca.entity.Livro;
-import br.com.escola.biblioteca.entity.Genero;
-import br.com.escola.biblioteca.entity.Editora;
 
 import java.util.List;
 
